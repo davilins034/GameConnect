7e465785195ef5df215ce2305a641f3ca2b37973
Davi Lins de Aráujo Melo <davilins034@gmail.com>
2025-10-14 15:41:34 -0300

    atualização

--- src/components/Cabeçalho.jsx
+++ src/components/Cabeçalho.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -7,16 +6,8 @@ function Header() {
       <div className="container header-container">
         <h1 className="logo-text">GAME CONNECT</h1>
         <nav className="header-nav">
-
-          {/* Navegação rápida */}
-          <Link to="/login" className="btn btn-secondary">
-            ENTRAR
-          </Link>
-
-          <Link to="/register" className="btn btn-primary">
-            CADASTRAR
-          </Link>
-
+          <button className="btn btn-secondary">ENTRAR</button>
+          <button className="btn btn-primary">CADASTRAR</button>
         </nav>
       </div>
     </header>
